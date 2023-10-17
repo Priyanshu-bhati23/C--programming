8fddd0c16d6819f718de4b42d62e6763e4ade57f
Priyanshu Bhati <138615018+Priyanshu-bhati23@users.noreply.github.com>
2023-10-17 21:53:11 +0530

    Add files via upload

---
+++ pta/let-s-C/index.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    printf("hello world");
+    int a,b;
+printf("enter first number");
+    scanf("%d",&a);
+    printf("enter second number");
+    scanf("%d",&b);
+        
+   int sum=a+b;
+   printf("the sum is");
+   printf("%d",sum);
+}
---
+++ pta/o.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,age=17;
+    printf("hello my name  is priyanshu and i am  %d years old \n",age);
+    scanf("%d %d",&a,&b);
+    int sum =a+b;
+    int product =a*b;
+    int floor_division =a/b;
+    int modulo =a%b;
+    int subtarction =a-b;
+printf("the outputs are %d %d %d %d %d ",sum,product,floor_division,modulo,subtarction);
+}
